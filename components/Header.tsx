cc8117d47f160122dfc554d52c64546e39e152b1
Zain <zainhussaini9898@gmail.com>
2024-09-28 05:14:43 +0530

    changed logo style

--- components/Header.tsx
+++ components/Header.tsx
@@ -92,8 +92,8 @@ const Header = () => {
               w-[200px]
               drop-shadow-[0_0_15px_rgba(255,255,255,0.6)]   /* Glow effect */
               md:drop-shadow-none   /* Remove glow on medium and larger screens */
-              animate-pulse        /* Optional pulsing effect for the glow */
-              md:animate-none      /* Remove animation on medium and larger screens */
+                    /* Optional pulsing effect for the glow */
+                    /* Remove animation on medium and larger screens */
             "
             priority
           />
