50838e7a576b6ade5bf3f9540edaeede7b49df4b
Zain <zainhussaini9898@gmail.com>
2025-01-24 01:50:38 +0530

    build optimisation done

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,14 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
+  output: "export",
+  images: { unoptimized: true },
 };
 
-export default config;
+export default nextConfig;
